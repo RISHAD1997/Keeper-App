a0eff0b3f9f52ab744a72bf95d982d2ffecd5973
Rishad <73650090+RISHAD1997@users.noreply.github.com>
2021-12-28 15:38:59 +0530

    second commit

---
+++ precache-manifest.0676cdb94c77b057dcef084352bb7152.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dda3e0d6ef21c3d5c3dcbd764329038",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "/Keeper-App/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "8f5af86f8e619fda999a",
+    "url": "/Keeper-App/static/js/main.9ace61c5.chunk.js"
+  },
+  {
+    "revision": "450f8c8ae8899abc2755",
+    "url": "/Keeper-App/static/js/runtime-main.d50d7308.js"
+  }
+]);
